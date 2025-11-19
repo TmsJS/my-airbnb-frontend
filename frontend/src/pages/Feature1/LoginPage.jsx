c912fa07359718dce70918e8a023a900dd1a7d1c
JianShao <jianshao5@gmail.com>
2025-11-20 02:05:17 +1100

    Merge from Sam & Add Enter key function

---
+++ frontend/src/pages/Feature1/LoginPage.jsx
@@ -0,0 +1,90 @@
+import { useState } from 'react';
+import { Link , useNavigate } from 'react-router-dom';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import axios from 'axios';
+import Alert from '@mui/material/Alert';
+// 2.1.1. Login Screen
+
+// A unique route must exist for this screen
+// 1.User must be able to enter their [email and password].
+// 2.If the form submission fails, a reasonable [error message] is shown
+// 3.A [button] must exist to allow [submission] of form
+// 4.The form must be able to be submitted on [enter key] in any of the fields
+
+function LoginPage(props) {
+  // 1.User must be able to enter their email and password.
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const navigate = useNavigate();
+  const [message, setMessage] = useState(''); 
+  const [isError, setIsError] = useState(false);
+
+    const handleCloseAlert = () => {
+      setIsError(false);
+      setMessage("");
+      setPassword("");
+      setEmail("");
+  };
+
+    const submit = async () => {
+        const bodyObj = {email,password};
+        try{
+            const response = await axios.post('http://localhost:5005/user/auth/login', bodyObj)
+            localStorage.setItem('token', response.data.token);
+            props.setToken(response.data.token);
+            navigate('/dashboard');
+
+        // 2.If the form submission fails, a reasonable error message is shown
+        }catch (error) {
+            setMessage("status " + error.status + " INVALID CREDENTIALS");
+            setIsError(true);  
+            console.log(error.message);
+        }
+    }
+
+    // 4.The form must be able to be submitted on [enter key] in any of the fields
+    const handleKeyDown = (e) => {
+      if (e.key === "Enter") {
+        submit(); 
+      }
+    };
+
+
+  return (
+    <div>
+      <br />
+        {isError && (
+              <Alert severity="error" onClose={handleCloseAlert}>{message}
+              </Alert>
+          )}
+        <br />
+        <TextField 
+            id="login-email" 
+            label="Email" 
+            variant="outlined"
+            value={email}
+            onChange={(event) => setEmail(event.target.value)}
+            onKeyDown={handleKeyDown}
+        /> 
+        <br />
+        <TextField 
+            id="login-password" 
+            label ="Password" 
+            type="password" 
+            variant="outlined"
+            value={password}
+            onChange={(event) => setPassword(event.target.value)}
+            onKeyDown={handleKeyDown}
+        /> 
+        <br /><br/>
+         {/*3.A button must exist to allow submission of form*/}
+         <Button onClick={submit} variant="outlined" color="secondary">Submit</Button>
+        <br /> <br/>
+        <Link to="/register">Not Registered? Register Now</Link>
+    </div>
+  )
+}
+
+export default LoginPage
