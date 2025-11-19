eb580d763fcb9499e61ab165e90c8b0788f68ba0
JianShao <jianshao5@gmail.com>
2025-11-20 06:30:06 +1100

    2.2.5. Publishing a listing

--- frontend/src/pages/Feature1/LoginPage.jsx
+++ frontend/src/pages/Feature1/LoginPage.jsx
@@ -21,93 +21,93 @@ function LoginPage(props) {
   const [message, setMessage] = useState(''); 
   const [isError, setIsError] = useState(false);
 
-    const handleCloseAlert = () => {
-      setIsError(false);
-      setMessage("");
-      setPassword("");
-      setEmail("");
+  const handleCloseAlert = () => {
+    setIsError(false);
+    setMessage("");
+    setPassword("");
+    setEmail("");
   };
     // Submit function 
     // 3.A [button] must exist to allow [submission] of form
-    const submit = async () => {
-      const bodyObj = { email, password };
+  const submit = async () => {
+    const bodyObj = { email, password };
 
-      try {
-        const response = await axios.post(
-          'http://localhost:5005/user/auth/login',bodyObj);
+    try {
+      const response = await axios.post(
+        'http://localhost:5005/user/auth/login',bodyObj);
 
-        localStorage.setItem('token', response.data.token);
-        localStorage.setItem("email", email); 
-        props.setToken(response.data.token);
-        navigate('/dashboard');
+      localStorage.setItem('token', response.data.token);
+      localStorage.setItem("email", email); 
+      props.setToken(response.data.token);
+      navigate('/dashboard');
 
-      } catch (error) {
-        let msg = "";
+    } catch (error) {
+      let msg = "";
 
-        if (!error.response) {
-          // Network connection error
-          msg = "Unable to connect to server, please check your network.";
+      if (!error.response) {
+        // Network connection error
+        msg = "Unable to connect to server, please check your network.";
+      } else {
+        const status = error.response.status;
+
+        if (status === 400) {
+          msg = "Invalid email or password.";
+        } else if (status === 403) {
+          msg = "You are not allowed to log in.";
+        } else if (status >= 500) {
+          msg = "Server error. Please try again later.";
+        } else if (error.response.data?.error) {
+          msg = error.response.data.error;  // From backend
         } else {
-          const status = error.response.status;
-
-          if (status === 400) {
-            msg = "Invalid email or password.";
-          } else if (status === 403) {
-            msg = "You are not allowed to log in.";
-          } else if (status >= 500) {
-            msg = "Server error. Please try again later.";
-          } else if (error.response.data?.error) {
-            msg = error.response.data.error;  // From backend
-          } else {
-            msg = `Unexpected error (status ${status}).`;
-          }
+          msg = `Unexpected error (status ${status}).`;
         }
-
-        setMessage(msg);
-        setIsError(true);
       }
-    };
+
+      setMessage(msg);
+      setIsError(true);
+    }
+  };
 
 
-    // 4.The form must be able to be submitted on [enter key] in any of the fields
-    const handleKeyDown = (e) => {
-      if (e.key === "Enter") {
-        submit(); 
-      }
-    };
+  // 4.The form must be able to be submitted on [enter key] in any of the fields
+  const handleKeyDown = (e) => {
+    if (e.key === "Enter") {
+      submit(); 
+    }
+  };
 
 
   return (
     <div>
       <br />
-        {isError && (
-              <Alert severity="error" onClose={handleCloseAlert}>{message}
-              </Alert>
-          )}
-        <br />
-        <TextField 
-            id="login-email" 
-            label="Email" 
-            variant="outlined"
-            value={email}
-            onChange={(event) => setEmail(event.target.value)}
-            onKeyDown={handleKeyDown}
-        /> 
-        <br />
-        <TextField 
-            id="login-password" 
-            label ="Password" 
-            type="password" 
-            variant="outlined"
-            value={password}
-            onChange={(event) => setPassword(event.target.value)}
-            onKeyDown={handleKeyDown}
-        /> 
-        <br /><br/>
-         {/*3.A button must exist to allow submission of form*/}
-         <Button onClick={submit} variant="outlined" color="secondary">Submit</Button>
-        <br /> <br/>
-        <Link to="/register">Not Registered? Register Now</Link>
+      {isError && (
+        <Alert severity="error" onClose={handleCloseAlert}>{message}
+        </Alert>
+      )}
+      <br />
+      <TextField 
+        id="login-email" 
+        label="Email" 
+        variant="outlined"
+        value={email}
+        onChange={(event) => setEmail(event.target.value)}
+        onKeyDown={handleKeyDown}
+      /> 
+      <br />
+      <TextField 
+        id="login-password" 
+        label ="Password" 
+        type="password" 
+        variant="outlined"
+        value={password}
+        onChange={(event) => setPassword(event.target.value)}
+        onKeyDown={handleKeyDown}
+      /> 
+      <br /><br/>
+      {/*3.A button must exist to allow submission of form*/}
+      <Button onClick={submit} variant="outlined" color="secondary">Submit</Button>
+      <br /> <br/>
+      <Link to="/register">Not Registered? Register Now</Link>
     </div>
   )
 }
